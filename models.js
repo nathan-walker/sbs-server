b24a84d614e347bfee51bf830ab9e618181ea1d5
Nathan Walker <me@nwalker.org>
2014-12-06 15:54:13 -0500

    Still working on this bug

--- models.js
+++ models.js
@@ -82,7 +82,7 @@ postSchema.methods.addTags = function(tagString, callback) {
 			item = item.toLowerCase();
 			Tag.findOne({ name: item }, function(err, tag) {
 				console.log(tag);
-				if (tag !== undefined || tag !== null) {
+				if (tag) {
 					console.log("up here, tag:" + tag);
 					cb(err, tag);
 				} else {
